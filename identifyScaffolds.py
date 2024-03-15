c1c67689830aaf90d5c3f0fa4b6873f2a12c92ad
grewelle <46788896+grewelle@users.noreply.github.com>
2024-03-14 19:25:19 -0700

    Add files via upload

---
+++ identifyScaffolds.py
@@ -0,0 +1,24 @@
+"""
+E. diaphana genome in silico digest with BCG1 enzyme
+Find line numbers of scaffolds to optimize runtime
+11-22-22
+Richard Grewelle
+"""
+
+def main():
+    """scaffoldID = '>'
+
+    filename = "C:/Users/Richard/Downloads/aiptasiaGenome.fna"
+
+    scaffoldList = []
+
+    with open(filename) as myFile:
+        for num, line in enumerate(myFile, 1):
+            if scaffoldID in line:
+                scaffoldList.append(num)
+    print(scaffoldList)"""
+
+
+
+
+main()
